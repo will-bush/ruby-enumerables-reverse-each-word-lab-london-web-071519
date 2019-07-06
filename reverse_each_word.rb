b2d3d95e063919ada159f1ddac319c12c1c079a5
Will Bush <williamdcbush@gmail.com>
2019-07-06 09:57:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,6 @@
 sentence = "Hello there, and how are you?"
 
 def reverse_each_word(sentence)
-
-array = sentence.split(" ")
-new_array = []
-
-array.each do |sentence|
-  new_array << sentence.reverse
-end
-new_array.join(" ")
-end
-
-def reverse_each_word_with_collect(sentence)
 array = sentence.split(" ")
 new_array = []
 
